89cce8ba5d077615a0d12a2ce2fbf3161672476b
inkfid <dominic.fidler@me.com>
2025-08-26 16:51:10 +0100

    Update index.js
    add code for deliveries

--- visitor-kiosk/index.js
+++ visitor-kiosk/index.js
@@ -324,6 +324,38 @@ const dataModel = {
     console.log('created', url);
     return url;
   }
+  sendDeliveryMessage() {
+    const token = this.getToken();
+    cost roomId = "Y2lzY29zcGFyazovL3VzL1JPT00vODBlY2Q5YjAtNTk5MC0xMWVhLWFlZGQtOGQxMWJmYzkxNGNm"
+    const message = "A delivery has arrived at reception.';
+
+    if (!token) {
+      alert("No Webex token available!");
+      return;
+    }
+    
+    fetch("https://webexapis.com/v1/messages", {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + token,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      roomId: roomId,
+      text: message
+    })
+  })
+    .then((response) => {
+      if (!response.ok) throw new Error("Failed to send message");
+      return response.json();
+    })
+    .then(() => {
+      alert("Delivery notification sent!");
+    })
+    .catch((error) => {
+      alert("Failed to send delivery notification: " + error.message);
+    });
+},
 };
 
 
