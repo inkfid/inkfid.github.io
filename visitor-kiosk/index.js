ef0777949ed3b742be9bbc02adc77fc37ce47c93
inkfid <dominic.fidler@me.com>
2025-08-27 13:36:09 +0100

    Update index.js

--- visitor-kiosk/index.js
+++ visitor-kiosk/index.js
@@ -327,16 +327,16 @@ const dataModel = {
   
   // send a message to a pre-defined webex space for deliveries
   sendDeliveryMessage() {
-    const token = this.getToken();
-    cost roomId = "Y2lzY29zcGFyazovL3VzL1JPT00vODBlY2Q5YjAtNTk5MC0xMWVhLWFlZGQtOGQxMWJmYzkxNGNm"
-    const message = "A delivery has arrived at reception.';
+  const token = this.getToken();
+  const roomId = "Y2lzY29zcGFyazovL3VzL1JPT00vODBlY2Q5YjAtNTk5MC0xMWVhLWFlZGQtOGQxMWJmYzkxNGNm";
+  const message = "A delivery has arrived at reception.";
 
-    if (!token) {
-      alert("No Webex token available!");
-      return;
-    }
-    
-    fetch("https://webexapis.com/v1/messages", {
+  if (!token) {
+    alert("No Webex token available!");
+    return;
+  }
+
+  fetch("https://webexapis.com/v1/messages", {
     method: "POST",
     headers: {
       Authorization: "Bearer " + token,
