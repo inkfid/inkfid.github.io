9d152c5fc15a8a140f47a6e8d443ed1719617a56
inkfid <dominic.fidler@me.com>
2025-08-27 19:32:50 +0100

    Update index.js

--- visitor-kiosk/index.js
+++ visitor-kiosk/index.js
@@ -5,40 +5,6 @@ Details:
 * Name: **$name**
 * Email: **$email**
 `;
-  
-// send a message to a pre-defined webex space for deliveries
-sendDeliveryMessage() {
-  const token = this.getToken();
-  const roomId = "Y2lzY29zcGFyazovL3VzL1JPT00vODBlY2Q5YjAtNTk5MC0xMWVhLWFlZGQtOGQxMWJmYzkxNGNm";
-  const message = "A delivery has arrived at reception.";
-
-  if (!token) {
-    alert("No Webex token available!");
-    return;
-  }
-
-  fetch("https://webexapis.com/v1/messages", {
-    method: "POST",
-    headers: {
-      Authorization: "Bearer " + token,
-      "Content-Type": "application/json"
-   },
-    body: JSON.stringify({
-      roomId: roomId,
-      text: message
-    })
-  })
-    .then((response) => {
-      if (!response.ok) throw new Error("Failed to send message");
-      return response.json();
-    })
-    .then(() => {
-      alert("Delivery notification sent!");
-    })
-    .catch((error) => {
-      alert("Failed to send delivery notification: " + error.message);
-    });
-}
 
 const dataModel = {
 // home > checkIn > findHost > confirmHost > photo > confim > registered | checkOut > checkOutResult
@@ -95,7 +61,40 @@ const dataModel = {
     this.phoneNumber = '';
     clearInterval(this.photoTimer);
   },
+// send a message to a pre-defined webex space for deliveries
+sendDeliveryMessage() {
+  const token = this.getToken();
+  const roomId = "Y2lzY29zcGFyazovL3VzL1JPT00vODBlY2Q5YjAtNTk5MC0xMWVhLWFlZGQtOGQxMWJmYzkxNGNm";
+  const message = "A delivery has arrived at reception.";
 
+  if (!token) {
+    alert("No Webex token available!");
+    return;
+  }
+
+  fetch("https://webexapis.com/v1/messages", {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + token,
+      "Content-Type": "application/json"
+   },
+    body: JSON.stringify({
+      roomId: roomId,
+      text: message
+    })
+  })
+    .then((response) => {
+      if (!response.ok) throw new Error("Failed to send message");
+      return response.json();
+    })
+    .then(() => {
+      alert("Delivery notification sent!");
+    })
+    .catch((error) => {
+      alert("Failed to send delivery notification: " + error.message);
+    });
+},
+  
   call() {
     const defaultNumber = 'erica.talking@ivr.vc';
     const number = new URLSearchParams(location.search).get('reception') || defaultNumber;
