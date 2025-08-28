2020e0ee90d1f02d0cde9b959881964f41ef5820
inkfid <dominic.fidler@me.com>
2025-08-28 17:21:47 +0100

    Update index.js
    added lines 75 - 81

--- visitor-kiosk/index.js
+++ visitor-kiosk/index.js
@@ -70,6 +70,16 @@ const dataModel = {
     location.href = `tel:${number}`;
   },
 
+// Code for Send Delivery Message
+  
+sendDeliveryMessage() {
+  const token = this.getToken();
+  const roomId = "Y2lzY29zcGFyazovL3VzL1JPT00vODBlY2Q5YjAtNTk5MC0xMWVhLWFlZGQtOGQxMWJmYzkxNGNm";
+  const markdown = "A delivery has arrived at reception.";
+  const message = "A delivery has arrived at reception.";
+  sendDeliveryMessage(token, roomId, markdown, message);
+},
+
   // Removed below section to try and remove requirement for email address
  get validForm() {
    const emailPattern = /\w+@\w+/;
