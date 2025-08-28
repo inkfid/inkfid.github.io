a4fbd0b0f2fa75d99af8ce991efe88c837469180
inkfid <dominic.fidler@me.com>
2025-08-28 19:48:12 +0100

    Update utils.js
    Added new token for Delivery BOT

--- visitor-kiosk/utils.js
+++ visitor-kiosk/utils.js
@@ -1,5 +1,7 @@
 // utils.js Added by DF - a new file to create a central getToken paramter used for multiple functions
 
+const deliveryToken = "OTA4MDRhNjItMzQ3Yy00ZGIzLWEyNTYtODg1N2RjYmIyNjU1N2RlOTM3ODUtOTRk_PF84_895155b7-cc7a-426c-bb66-768bd3be1b8e"
+
 /**
  * Extracts the 'token' parameter from the URL query string.
  * @returns {string|null} The token value or null if not present.
