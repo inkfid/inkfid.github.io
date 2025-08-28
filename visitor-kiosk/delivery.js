257e2098f083b3f649f50137d34288966c25140c
inkfid <dominic.fidler@me.com>
2025-08-28 19:51:01 +0100

    Update delivery.js

--- visitor-kiosk/delivery.js
+++ visitor-kiosk/delivery.js
@@ -2,12 +2,13 @@
 // send a message to a pre-defined webex space for deliveries
 
 const webexMsgUrlDF = 'https://webexapis.com/v1/messages';
-const token = getToken();  // Assuming getToken is defined somewhere
+// this was working - const token = getToken();  // Assuming getToken is defined somewhere all references that are deliveryToken WERE token
+import deliveryToken from '.utils.js';
 const roomId = "Y2lzY29zcGFyazovL3VzL1JPT00vODBlY2Q5YjAtNTk5MC0xMWVhLWFlZGQtOGQxMWJmYzkxNGNm";
 const markdown = "A delivery has arrived at reception.";
 const message = "A parcel has been left at reception.";
 
-function sendDeliveryMessage(token, roomId, markdown, message) {
+function sendDeliveryMessage(deliveryToken, roomId, markdown, message) {
   const formData = new FormData();
   // formData.set('markdown', markdown);
   formData.set('roomId', roomId);
@@ -15,7 +16,7 @@ function sendDeliveryMessage(token, roomId, markdown, message) {
 
   const options = {
     headers: {
-      Authorization: 'Bearer ' + token,
+      Authorization: 'Bearer ' + deliveryToken,
     },
     method: 'POST',
     body: formData,
