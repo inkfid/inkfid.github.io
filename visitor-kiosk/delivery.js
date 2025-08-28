a759fc2390a1eb76dfcfd23caa132bc700fbdc16
inkfid <dominic.fidler@me.com>
2025-08-28 11:18:41 +0100

    Update delivery.js

--- visitor-kiosk/delivery.js
+++ visitor-kiosk/delivery.js
@@ -4,12 +4,13 @@
 const webexMsgUrlDF = 'https://webexapis.com/v1/messages';
 const token = this.getToken();
 const roomId = "Y2lzY29zcGFyazovL3VzL1JPT00vODBlY2Q5YjAtNTk5MC0xMWVhLWFlZGQtOGQxMWJmYzkxNGNm";
-// const message = "A delivery has arrived at reception.";
+const message = "A delivery has arrived at reception.";
 
-sendDeliveryMessage(token, roomId, markdown) {
+sendDeliveryMessage(token, roomId, markdown, message) {
   const formData = new FormData();
   formData.set('markdown', markdown);
   formData.set('roomId', roomId);
+  formData.set('text', message);
 
   const options = {
     headers: {
