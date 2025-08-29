5ddcb204f253e452da9b9cf9deb4a361f09a094f
inkfid <dominic.fidler@me.com>
2025-08-29 17:46:47 +0100

    Update index.js
    un-commented line 150

--- visitor-kiosk/index.js
+++ visitor-kiosk/index.js
@@ -147,7 +147,7 @@ notifyRecipient() {
       this.deliveryNoticeMessage = "Recipient notified.";
       this.deliveryNotice = true;
       this.page = 'deliveryConfirmed'; // Show confirmation page
-      // hide this to see if errors disappear | this.reset(); //Clear fields
+      this.reset(); //Clear fields
     })
     .catch(() => {
       this.deliveryNoticeMessage = "Failed to notify recipient.";
