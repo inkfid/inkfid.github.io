4ce434debc0ae7634c66644bc12a947b00295bda
inkfid <dominic.fidler@me.com>
2025-08-29 16:31:05 +0100

    Update index.js

--- visitor-kiosk/index.js
+++ visitor-kiosk/index.js
@@ -26,6 +26,7 @@ const dataModel = {
   videoStream: null,
   phoneNumber: '',
   taxiNumber: '',
+  selectedRecipient: null,
   mapUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d37964.479957946394!2d-121.95893677399364!3d37.41713987799405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fc911562d481f%3A0xd3d896b473be003!2sCisco%20Systems%20Building%2012!5e0!3m2!1sen!2sno!4v1674211511880!5m2!1sen!2sno',
 
   init() {
